21a31801f59e2ef5d1b1d57bd17ef82d0039d82f
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-10-07 21:51:17 -0300

    Alterando os Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    publication_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)  # Verifique se o nome do campo é 'author'
+    category = models.ForeignKey(Category, related_name="books", on_delete=models.CASCADE)
+    publication_date = models.DateTimeField()
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
