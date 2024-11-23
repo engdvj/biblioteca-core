170dff50da856333763d9fe76efda56bc4f2d4b4
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-11-22 23:59:28 -0300

    Atualizações finaiss

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    auth_token = request.COOKIES.get('auth_token')
+    user_name = "Usuário"  # Valor padrão caso o token não seja válido
+    if auth_token:
+        try:
+            token = Token.objects.get(key=auth_token)
+            user_name = token.user.username  # Obtém o nome do usuário a partir do token
+        except Token.DoesNotExist:
+            pass
+
+    return render(request, 'dashboard.html', {'user_name': user_name})
